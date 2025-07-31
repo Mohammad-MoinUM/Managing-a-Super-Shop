74800ff8c606c8d9249da155c77fa987f3f44858
Mohammad Moin Uddin Moin <mohammadmoinuddin2491@gmail.com>
2025-07-31 09:19:53 +0600

    liquid and shop class added

--- project.cpp
+++ project.cpp
@@ -62,3 +62,69 @@ public:
         return meat_name[n];
     }
 };
+class Liquid
+{
+    int i;
+    string liquid_name[3] = {"Oil", "Cocacola", "Mojo"};
+    float liquid_price[3];
+
+public:
+    Liquid()
+    {
+        this->liquid_price[0] = 200; // price[0];
+        this->liquid_price[1] = 30; // price[2];
+        this->liquid_price[2] = 40;
+    }
+    Liquid(int i, int t)
+    {
+        this->liquid_price[i] = t;
+    }
+
+    float getVal(int n)
+    {
+        return liquid_price[n];
+    }
+
+    string getName(int n)
+    {
+        return liquid_name[n];
+    }
+};
+
+class Manager
+{
+    string pass = "12345";
+
+public:
+    bool matchPass(string *str)
+    {
+        if (*str == pass)
+            return true;
+        return false;
+    }
+    friend Vegetables;
+    friend Meat;
+    friend Liquid;
+};
+
+class Shop
+{
+public:
+    Vegetables veg;
+    Meat meat;
+    Liquid lq;
+    Manager mn;
+    Shop() {}
+    void setVeg(int index, int n)
+    {
+        veg = Vegetables(index, n);
+    }
+    void setMeat(int index, int n)
+    {
+        meat = Meat(index, n);
+    }
+    void setLiq(int index, int n)
+    {
+        lq = Liquid(index, n);
+    }
+};
